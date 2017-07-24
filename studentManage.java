0bc8676e3064b4451f55dc1ed4f713669fe9e559
Yuvraj Batra <yuvrajbatra5@gmail.com>
2017-07-25 01:44:15 +0530

    added information required of the students

--- studentManage.java
+++ studentManage.java
@@ -2,9 +2,30 @@ package studentManager;
 
 public class studentManage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public String Name;
+	public int RollNumber;
 
+	public studentManage(String Name, int RollNumber) {
+		this.Name = Name;
+		this.RollNumber = RollNumber;
 	}
 
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public int getRollNumber() {
+		return RollNumber;
+	}
+
+	public void setRollNumber(int rollNumber) {
+		this.RollNumber = RollNumber;
+	}
+public void printInfo(){
+	System.out.println(getName()+ "your Roll Number is" +getRollNumber());
+}
 }
