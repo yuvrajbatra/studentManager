88bbdc42f5aac90571aa802bb62e541b0e0e99c9
yuvrajbatra <yuvrajbatra5@gmail.com>
2017-07-26 01:01:45 +0530

    Add files via upload

---
+++ src/student/student.java
@@ -0,0 +1,57 @@
+package student;
+
+public class student {
+
+	public String Name;
+	public int RollNumber;
+	public String Dob;
+	public int StudentClass;
+
+	public student(String name, int rollNumber, String dob, int StudentClass) {
+		this.Name = name;
+		this.RollNumber = rollNumber;
+		this.Dob = dob;
+		this.StudentClass = StudentClass;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		this.Name = name;
+	}
+
+	public int getRollNumber() {
+		return RollNumber;
+	}
+
+	public void setRollNumber(int rollNumber) {
+		this.RollNumber = rollNumber;
+	}
+
+	public String getDob() {
+		return Dob;
+	}
+
+	public void setDob(String dob) {
+		this.Dob = dob;
+	}
+
+	public int getStudentClass() {
+		return StudentClass;
+	}
+
+	public void setStudentClass(int studentClass) {
+		this.StudentClass = studentClass;
+	}
+
+	public void printInfo() {
+		System.out.println("name-" + " " + getName() + "     " + "Roll Number-" + " " + getRollNumber() + "     "
+				+ "Date of birth-" + getDob() + "     " + "Class-" + getStudentClass());
+	}
+
+	public class test {
+
+	}
+}
---
+++ src/student/test.java
@@ -0,0 +1,10 @@
+package student;
+
+public class test {
+	
+	public static void main(String[] args) {
+		student s1 = new student("ravi",21,"10/2/1998",10);
+		s1.printInfo();
+	}
+
+}
